dbb24df14df9d6e212ddc72107cc69c1fde2a11e
Sameer Kotecha <sameerzkotecha@gmail.com>
2024-04-08 19:30:54 -0500

    Created Flyout menu for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,14 +10,16 @@ const Navbar = () => {
     return (
         <>
         <nav className='bg-black'>
-            <div className='max-w-6xl mx-auto'>
+            <div className='max-w-7xl mx-auto'>
                 <div className='flex justify-between items-center px-3'>
                     <div>
                         <img src = {Logo} alt='logo' className='w-12' />
                     </div>
                     <div className='hidden md:flex text-white space-x-3'>
                         <a href="#" className='py-5 px-2 hover:text-gray-600'>Link 1</a>
-                        <a href="#" className='py-5 px-2 hover:text-gray-600'>Link 2</a>
+                        <NavLinks href='#' Flyoutcontent = {NavContent}>
+                            Link 2
+                        </NavLinks>
                         <a href="#" className='py-5 px-2 hover:text-gray-600'>Link 3</a>
                     </div> 
                     <div className='hidden md:flex'>
@@ -45,11 +47,51 @@ const Navbar = () => {
 }
 
 
-const NavLinks = () => {
+const NavLinks = ({children, href, Flyoutcontent}) => {
+
+    const [open, setOpen] = useState(false);
+
+    const showFlyout = open && Flyoutcontent;
+
+
+
     return(
-        <div>
-            
+        <div 
+        className='relative flex items-center'
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+
+        >
+            <a href={href} className='relative text-white'>
+                {children}
+                <span 
+                style={{
+                    transform: showFlyout ? "scaleX(1)" : "scaleX(0)"
+                }}
+                className='absolute -bottom-2 -left-2 -right-2 h-1 
+                origin-left rounded-full bg-indigo-300 transition-transform 
+                duration-300 ease-out' />
+            </a>
+            {showFlyout && (
+            <div className='absolute left-1/2 top-20 -translate-x-1/2 bg-white
+             text-black'>
+                <div className='absolute -top-8 left-0 right-0 h-6'> </div>
+                <div className='absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2
+                -translate-y-1/2 rotate-45 bg-purple-200'> </div>
+                <Flyoutcontent /> 
+            </div>
+            )}
         </div>
+    )
+}
+
+const NavContent = () => {
+    return(
+        <div className='min-h-64 w-48 rounded-lg bg-purple-200 p-6 shadow-xl flex flex-col justify-start gap-y-4'>
+        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 1</a>
+        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 2</a>
+        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 3</a>
+    </div>
     )
 }
 
