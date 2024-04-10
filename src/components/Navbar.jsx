c5c8a82cbf3e491c4a44cbd0988a4754bfa3866b
Sameer Kotecha <sameerzkotecha@gmail.com>
2024-04-10 14:03:31 -0500

    Created FlyOutMenu and redesigned mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,36 @@
 import Logo from '../assets/logo.svg'
 import Hamburger from '../assets/hamburger.svg'
 import Close from '../assets/close.svg'
+import IntegrationIcon from '../assets/integration.svg'
+import Opti from '../assets/opti.svg'
+import Custom from '../assets/custom.svg'
+import Visual from '../assets/visual.svg'
+import Dropdown from '../assets/dropdown.svg'
 import {useState} from 'react'
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isFlyOpen, setFlyOpen] = useState(false)
 
     return (
         <>
-        <nav className='bg-black'>
+        <nav className='relative'>
             <div className='max-w-7xl mx-auto'>
                 <div className='flex justify-between items-center px-3'>
                     <div>
-                        <img src = {Logo} alt='logo' className='w-12' />
+                        <a href = '#'>
+                            <img src = {Logo} alt='logo' className='w-12' />
+                        </a>
                     </div>
-                    <div className='hidden md:flex text-white space-x-3'>
-                        <a href="#" className='py-5 px-2 hover:text-gray-600'>Link 1</a>
-                        <NavLinks href='#' Flyoutcontent = {NavContent}>
-                            Link 2
-                        </NavLinks>
-                        <a href="#" className='py-5 px-2 hover:text-gray-600'>Link 3</a>
+                    <div className='hidden md:flex text-black space-x-3'>
+                        <a href="#" className='py-5 px-2 hover:text-gray-600'>About Us</a>
+                        <a href="#" className='py-5 px-2 hover:text-gray-600 pr-1 flex items-center' onClick={() => setFlyOpen(!isFlyOpen)}>
+                            Products
+                            <img src={Dropdown} className='w-5 cursor-pointer' onClick={() => setFlyOpen(!isFlyOpen)} />
+                        </a>
+                        <a href="#" className='py-5 px-2 hover:text-gray-600'>Blog</a>
+                        <a href="#" className='py-5 px-2 hover:text-gray-600'>Contact Us</a>
                     </div> 
                     <div className='hidden md:flex'>
                         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Call to Action</button>
@@ -33,65 +43,66 @@ const Navbar = () => {
                 </div>
             </div>
             {isOpen && (
-                <div className='text-white'>
-                    <a href="#" className='block py-3 px-2 hover:bg-gray-500 text-sm'>Link 1</a>
-                    <a href="#" className='block py-3 px-2 hover:bg-gray-500 text-sm'>Link 2</a>
-                    <a href="#" className='block py-3 px-2 hover:bg-gray-500 text-sm'>Link 3</a>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded mb-3 ml-1'>Call to Action</button>
+                <div className='text-black md:hidden'>
+                    <a href="#" className='block py-3 px-2 text-md text-center hover:text-gray-600'>About Us</a>
+                    <hr className="my-3 h-0.5 w-1/2 border-t-0 bg-gray-300 mx-auto" />
+                    <a href="#" className='block py-3 px-2 hover:text-gray-600 text-md text-center'>Products</a>
+                    <hr className="my-3 h-0.5 w-1/2 border-t-0 bg-gray-300 mx-auto" />
+                    <a href="#" className='block py-3 px-2 hover:text-gray-600 text-md text-center'>Blog</a>
+                    <hr className="my-3 h-0.5 w-1/2 border-t-0 bg-gray-300 mx-auto" />
+                    <a href="#" className='block py-3 px-2 hover:text-gray-600 text-md text-center'>Contact Us</a>
+                    <hr className="my-3 h-0.5 w-1/2 border-t-0 bg-gray-300 mx-auto" />
+                    <a href="#" className='block py-3 px-2 hover:text-gray-600 text-md text-center'>Log in</a>
                 </div>
             )}
-            
+            {isFlyOpen && <FlyOut />}
         </nav>
+
         </>
     )
 }
 
-
-const NavLinks = ({children, href, Flyoutcontent}) => {
-
-    const [open, setOpen] = useState(false);
-
-    const showFlyout = open && Flyoutcontent;
-
-
-
+const FlyOut = () => {
     return(
-        <div 
-        className='relative flex items-center'
-        onMouseEnter={() => setOpen(true)}
-        onMouseLeave={() => setOpen(false)}
-
-        >
-            <a href={href} className='relative text-white'>
-                {children}
-                <span 
-                style={{
-                    transform: showFlyout ? "scaleX(1)" : "scaleX(0)"
-                }}
-                className='absolute -bottom-2 -left-2 -right-2 h-1 
-                origin-left rounded-full bg-indigo-300 transition-transform 
-                duration-300 ease-out' />
-            </a>
-            {showFlyout && (
-            <div className='absolute left-1/2 top-20 -translate-x-1/2 bg-white
-             text-black'>
-                <div className='absolute -top-8 left-0 right-0 h-6'> </div>
-                <div className='absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2
-                -translate-y-1/2 rotate-45 bg-purple-200'> </div>
-                <Flyoutcontent /> 
+        <div className='w-screen max-w-sm bg-white rounded-3xl shadow-lg border-solid border-2 absolute left-1/2 
+        transform -translate-x-1/2 z-10'>
+            <div className='p-4 space-y-3'>
+                <FlyOutItem
+                    src = {IntegrationIcon}
+                    title = "Integration"
+                    desc = "Seamlessly connect all your different systems"
+                />
+                <FlyOutItem
+                    src={Opti}
+                    title= "Optimization"
+                    desc = "Improve effciency of processes or systems"
+                />
+                <FlyOutItem 
+                src={Custom}
+                title= "Customization"
+                desc = "Tailor your business to specific needs"
+            />
+            <FlyOutItem 
+                src={Visual}
+                title= "Visualization"
+                desc = "Represent your data in a way you can see"
+            />
             </div>
-            )}
         </div>
     )
 }
 
-const NavContent = () => {
-    return(
-        <div className='min-h-64 w-48 rounded-lg bg-purple-200 p-6 shadow-xl flex flex-col justify-start gap-y-4'>
-        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 1</a>
-        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 2</a>
-        <a href='#' className='w-full text-center py-2  rounded hover:bg-purple-300 transition-colors duration-150'>Sublink 3</a>
-    </div>
+const FlyOutItem = ( {src, title, desc}) => {
+    return (
+        <div className="flex items-center space-x-6 hover:bg-gray-100 rounded-lg p-3 cursor-pointer">
+            <div className='w-8 h-8 flex items-center justify-center'>
+                <img src={src} alt='logo' className='w-6' />
+            </div>
+            <div className='space-y-1'>       
+                <h1 className="text-md font-bold">{title}</h1>
+                <p className="text-sm">{desc}</p>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Scratch.jsx
@@ -0,0 +1,4 @@
+<div className='flex items-center'>
+                            <a href="#" className='py-5 px-2 hover:text-gray-600 pr-1' onClick={() => setFlyOpen(!isFlyOpen)}>Products</a>
+                            <img src={Dropdown} className='w-5 flex-none cursor-pointer' onClick={() => setFlyOpen(!isFlyOpen)} />
+                        </div>
